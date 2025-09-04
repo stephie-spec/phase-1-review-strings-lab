101dc3c3ab5579c1c2f40a9208645a2e576cc06c
stephie-spec <compwork254@gmail.com>
2025-09-04 08:34:07 +0300

    complete strings lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = "Grace Hopper";
+// const welcomeMessage = "Welcome to Flatbook, ";
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// const shortGreeting = `Welcome, ${currentUser}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
